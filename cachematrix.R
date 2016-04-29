9bcff72cab4c8a2608ca5b18070acbd0f57652b3
Xialei <leixiafd@126.com>
2016-04-29 11:48:19 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	x_inverse <- NULL
+	set <- function( y ){
+		x <<- y
+		x_inverse <<- NULL
+	}
+	get <- function() x
+	setinverse <- function( inverse ) x_inverse <<- inverse
+	getinverse <- function() x_inverse
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse )
 }
 
 
@@ -12,4 +20,25 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		x_inverse <- x$getinverse()
+		if( !is.null( x_inverse ) ){
+			message( "getting the cached inverse" )
+			return( x_inverse )
+		}
+		xmatrix <- x$get()
+		x_inverse <- solve( xmatrix, ... )
+		x$setinverse( x_inverse )
+		x_inverse
 }
+# uncomment below lines, and run this file with "Rscript cachematrix.R" in your linux shell, gives a quick test of this script.
+"
+x <- makeCacheMatrix( matrix( 1:4, nrow = 2, ncol = 2 ) )
+x1 <- cacheSolve(x)
+y <- matrix( 1:4, nrow = 2, ncol = 2 )
+y1 <- solve(y)
+if( identical(x1,y1) ){
+	print( "x1 == y1")
+}else{
+	print( "x1 != y1" )
+}
+"
